09b0852e76870945a41bd60bc8f7424fb051bf6e
Reynaldo Perez Pauli <reynaldoperezpauli6@gmail.com>
2024-08-26 00:29:40 -0500

    Update Project

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const user = require('./user');
 const { authentication, validateUserBody } = require('../middlewares/validation');
 const NotFoundError = require('../errors/not-found-err');
 
-// COULD YOU HELP ME FIGURE OUT WHAT THE PROBLEM MIGHT BE. THANKS
+// COULD YOU HELP ME FIGURE OUT WHAT THE PROBLEM MIGHT BE, PLEASE?. I'VE BEEN STRUGGLING FOR HOURS. I'D BE REALLY THANKFUL
 router.post('/signin', authentication, login);
 router.post('/signup', validateUserBody, createUser);
 router.use('/users', user);
